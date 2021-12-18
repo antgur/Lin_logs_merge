abef18e7df78b40ad9a14e0b8cb3fbc2ef56194a
antgur <antgur@gmail.com>
2021-12-18 15:56:35 +0200

    Commit

--- main.h
+++ main.h
@@ -1,7 +1,15 @@
 #if !defined(MAIN1_H)
 #define MAIN1_H
 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/un.h>
+#include <pigpio.h>
 
 #include "utils/srvlog.h"
 #include "utils/ipcr.h"
